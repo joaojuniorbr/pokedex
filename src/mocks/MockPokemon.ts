0774b9dbf493e287eca78b18294b47051e03c1c1
Joao Junior <joaojr@my.com>
2024-09-25 21:34:26 -0300

    :zap: mock pokemon data

--- src/mocks/MockPokemon.ts
+++ src/mocks/MockPokemon.ts
@@ -1,21 +1,49 @@
+const createAbility = (
+	name: string,
+	url: string,
+	is_hidden: boolean,
+	slot: number
+) => ({
+	ability: { name, url },
+	is_hidden,
+	slot,
+});
+
+const createVersionDetails = (
+	game_index: number,
+	version_name: string,
+	version_url: string
+) => ({
+	game_index,
+	version: { name: version_name, url: version_url },
+});
+
+const createSprite = (
+	front_default: string,
+	back_default: string,
+	front_shiny: string,
+	back_shiny: string
+) => ({
+	front_default,
+	back_default,
+	front_shiny,
+	back_shiny,
+});
+
 export const mockPokemon = {
 	abilities: [
-		{
-			ability: {
-				name: 'overgrow',
-				url: 'https://pokeapi.co/api/v2/ability/65/',
-			},
-			is_hidden: false,
-			slot: 1,
-		},
-		{
-			ability: {
-				name: 'chlorophyll',
-				url: 'https://pokeapi.co/api/v2/ability/34/',
-			},
-			is_hidden: true,
-			slot: 3,
-		},
+		createAbility(
+			'overgrow',
+			'https://pokeapi.co/api/v2/ability/65/',
+			false,
+			1
+		),
+		createAbility(
+			'chlorophyll',
+			'https://pokeapi.co/api/v2/ability/34/',
+			true,
+			3
+		),
 	],
 	base_experience: 64,
 	cries: {
@@ -28,134 +56,42 @@ export const mockPokemon = {
 		{ name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon-form/1/' },
 	],
 	game_indices: [
-		{
-			game_index: 153,
-			version: { name: 'red', url: 'https://pokeapi.co/api/v2/version/1/' },
-		},
-		{
-			game_index: 153,
-			version: { name: 'blue', url: 'https://pokeapi.co/api/v2/version/2/' },
-		},
-		{
-			game_index: 153,
-			version: {
-				name: 'yellow',
-				url: 'https://pokeapi.co/api/v2/version/3/',
-			},
-		},
-		{
-			game_index: 1,
-			version: { name: 'gold', url: 'https://pokeapi.co/api/v2/version/4/' },
-		},
-		{
-			game_index: 1,
-			version: {
-				name: 'silver',
-				url: 'https://pokeapi.co/api/v2/version/5/',
-			},
-		},
-		{
-			game_index: 1,
-			version: {
-				name: 'crystal',
-				url: 'https://pokeapi.co/api/v2/version/6/',
-			},
-		},
-		{
-			game_index: 1,
-			version: { name: 'ruby', url: 'https://pokeapi.co/api/v2/version/7/' },
-		},
-		{
-			game_index: 1,
-			version: {
-				name: 'sapphire',
-				url: 'https://pokeapi.co/api/v2/version/8/',
-			},
-		},
-		{
-			game_index: 1,
-			version: {
-				name: 'emerald',
-				url: 'https://pokeapi.co/api/v2/version/9/',
-			},
-		},
-		{
-			game_index: 1,
-			version: {
-				name: 'firered',
-				url: 'https://pokeapi.co/api/v2/version/10/',
-			},
-		},
-		{
-			game_index: 1,
-			version: {
-				name: 'leafgreen',
-				url: 'https://pokeapi.co/api/v2/version/11/',
-			},
-		},
-		{
-			game_index: 1,
-			version: {
-				name: 'diamond',
-				url: 'https://pokeapi.co/api/v2/version/12/',
-			},
-		},
-		{
-			game_index: 1,
-			version: {
-				name: 'pearl',
-				url: 'https://pokeapi.co/api/v2/version/13/',
-			},
-		},
-		{
-			game_index: 1,
-			version: {
-				name: 'platinum',
-				url: 'https://pokeapi.co/api/v2/version/14/',
-			},
-		},
-		{
-			game_index: 1,
-			version: {
-				name: 'heartgold',
-				url: 'https://pokeapi.co/api/v2/version/15/',
-			},
-		},
-		{
-			game_index: 1,
-			version: {
-				name: 'soulsilver',
-				url: 'https://pokeapi.co/api/v2/version/16/',
-			},
-		},
-		{
-			game_index: 1,
-			version: {
-				name: 'black',
-				url: 'https://pokeapi.co/api/v2/version/17/',
-			},
-		},
-		{
-			game_index: 1,
-			version: {
-				name: 'white',
-				url: 'https://pokeapi.co/api/v2/version/18/',
-			},
-		},
-		{
-			game_index: 1,
-			version: {
-				name: 'black-2',
-				url: 'https://pokeapi.co/api/v2/version/21/',
-			},
-		},
-		{
-			game_index: 1,
-			version: {
-				name: 'white-2',
-				url: 'https://pokeapi.co/api/v2/version/22/',
-			},
-		},
+		createVersionDetails(153, 'red', 'https://pokeapi.co/api/v2/version/1/'),
+		createVersionDetails(153, 'blue', 'https://pokeapi.co/api/v2/version/2/'),
+		createVersionDetails(153, 'yellow', 'https://pokeapi.co/api/v2/version/3/'),
+		createVersionDetails(1, 'gold', 'https://pokeapi.co/api/v2/version/4/'),
+		createVersionDetails(1, 'silver', 'https://pokeapi.co/api/v2/version/5/'),
+		createVersionDetails(1, 'crystal', 'https://pokeapi.co/api/v2/version/6/'),
+		createVersionDetails(1, 'ruby', 'https://pokeapi.co/api/v2/version/7/'),
+		createVersionDetails(1, 'sapphire', 'https://pokeapi.co/api/v2/version/8/'),
+		createVersionDetails(1, 'emerald', 'https://pokeapi.co/api/v2/version/9/'),
+		createVersionDetails(1, 'firered', 'https://pokeapi.co/api/v2/version/10/'),
+		createVersionDetails(
+			1,
+			'leafgreen',
+			'https://pokeapi.co/api/v2/version/11/'
+		),
+		createVersionDetails(1, 'diamond', 'https://pokeapi.co/api/v2/version/12/'),
+		createVersionDetails(1, 'pearl', 'https://pokeapi.co/api/v2/version/13/'),
+		createVersionDetails(
+			1,
+			'platinum',
+			'https://pokeapi.co/api/v2/version/14/'
+		),
+		createVersionDetails(
+			1,
+			'heartgold',
+			'https://pokeapi.co/api/v2/version/15/'
+		),
+		createVersionDetails(
+			1,
+			'soulsilver',
+			'https://pokeapi.co/api/v2/version/16/'
+		),
+		createVersionDetails(1, 'black', 'https://pokeapi.co/api/v2/version/17/'),
+		createVersionDetails(1, 'white', 'https://pokeapi.co/api/v2/version/18/'),
+		createVersionDetails(1, 'black-2', 'https://pokeapi.co/api/v2/version/21/'),
+		createVersionDetails(1, 'white-2', 'https://pokeapi.co/api/v2/version/22/'),
 	],
 	height: 7,
 	held_items: [],
@@ -200,18 +136,12 @@ export const mockPokemon = {
 		url: 'https://pokeapi.co/api/v2/pokemon-species/1/',
 	},
 	sprites: {
-		back_default:
-			'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png',
-		back_female: null,
-		back_shiny:
-			'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png',
-		back_shiny_female: null,
-		front_default:
+		...createSprite(
 			'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
-		front_female: null,
-		front_shiny:
+			'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png',
 			'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png',
-		front_shiny_female: null,
+			'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png'
+		),
 		other: {
 			dream_world: {
 				front_default:
